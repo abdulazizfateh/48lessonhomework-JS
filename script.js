a2dd97fb6f76d7befb63bd04576ab386ee113423
Abdulaziz Ergashev <eabdulaziz36@gmail.com>
2024-07-05 09:28:56 +0500

    Review

---
+++ script.js
@@ -0,0 +1,84 @@
+// 1. array ichidagi eng katta sonni toping 
+// 2. array ichidagi eng kichik sonni toping 
+// 3. Foydalanuvchi son kiritadi va arrey ichidagi foydalanuvchi sonidan kichik bo’lgan sonlarni ekranga chiqaring 
+// 4. argument sifatida qabul qilingan sonning mukammal yoki mukammal son emasligini aniqlaydigan function yarating 
+// 5. raqamlar kiritilgan arreyni teskari qilish (for dan foydalaning) 
+// 6. arrey ichidagi tub sonlar ni toping 
+// 7. Ixtiyoriy function va value lari sonlardan iborat bo'lgan object yaratilsin, va object value laridan tashkil topgan arrayga function parametri ham oxiridan ham boshidan qo'shilsin
+
+
+
+
+
+
+// TASK1
+
+
+let array = [1, 11, 20, 9, 71];
+let maxNum = Math.max(...array);
+console.log("Eng katta son:", maxNum);
+
+
+
+// TASK2
+
+
+
+
+
+
+
+
+
+
+
+
+// TASK3
+
+
+
+
+
+
+
+
+
+
+
+// TASK4
+
+
+
+
+
+
+
+
+
+
+// TASK5
+
+
+
+
+
+
+
+
+
+
+
+
+
+// TASK6
+
+
+
+
+
+
+
+
+
+
+// TASK7
